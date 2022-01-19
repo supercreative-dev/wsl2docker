438f8317333558f4dcaae29ece36bb472b9721a4
Hojun Choi <corbie79@msn.com>
2022-01-19 14:49:56 +0900

    apt-get 에  -y 옵션 기본 추가

--- wsl2_docker_install.sh
+++ wsl2_docker_install.sh
@@ -6,16 +6,17 @@ sudo tee -a /etc/wsl.conf > /dev/null <<EOT
 [boot]
 command = service docker start
 EOT
-sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
 
 #도커 apt 소스 리스트 등록 우분투
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
 
 # 도커 설치
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get update -y
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 # docker compose 설치
 # v2.2.3
